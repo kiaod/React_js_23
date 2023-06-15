f282d513250b7d9eb93b87310ea6659aeba1bcf9
Ivan <od.ua.kia@gmail.com>
2023-06-15 16:35:21 +0300

    Component states

--- React App/my-app/src/App.js
+++ React App/my-app/src/App.js
@@ -1,19 +1,38 @@
+/* eslint-disable no-useless-constructor */
+import { Component } from 'react';
+
 import './App.css';
 
-function WhoAmI ({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name()}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+class WhoAmI extends  Component {
+  state = {
+    years: 27,
+    text: '+++'
+  };
+
+  nextYear = () => {
+    console.log('+++');
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name={() => {return 'John'}} surname="Smith" link="Facebook.com"/>
-      <WhoAmI name={() => {return 'John'}} surname="Koval" link="vk.com"/>
+      <WhoAmI name={'John'} surname="Smith" link="Facebook.com"/>
+      <WhoAmI name={'Alex'} surname="Koval" link="vk.com"/>
     </div>
   );
 }
