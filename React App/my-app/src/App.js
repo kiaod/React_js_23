e6e22d20f9ffa3909e3ab2090a6b534bdd0de0ba
Ivan <od.ua.kia@gmail.com>
2023-06-16 17:40:05 +0300

    events in React

--- React App/my-app/src/App.js
+++ React App/my-app/src/App.js
@@ -4,25 +4,37 @@ import { Component } from 'react';
 import './App.css';
 
 class WhoAmI extends  Component {
-  state = {
-    years: 27,
-    text: '+++'
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      position: ''
+    }
+  }
   nextYear = () => {
-    console.log('+++');
     this.setState(state => ({
       years: state.years + 1
     }))
   }
 
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
   render() {
     const {name, surname, link} = this.props;
+    const {position, years} = this.state;
     return (
       <div>
-        <button onClick={this.nextYear}>{this.state.text}</button>
-        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <button onClick={this.nextYear}>+++</button>
+        <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
         <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInputChanges} />
+        </form>
       </div>
     )
   }
